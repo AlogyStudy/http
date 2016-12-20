cb4999751b1b1b0bc5e9b1dbc37e8d1b3e9a04a0
Xingzhang Lin <1129507496@qq.com>
2016-12-20 22:23:51 +0800

    change ajax upload

--- ajax/upfile.php
+++ ajax/upfile.php
@@ -2,12 +2,8 @@
 	
 //	print_r($_POST);
 	
-	if ( !empty($_POST) ) {
-		file_put_contents('./upfile.txt', $_POST);
-		echo '上传成功';
-	} else {
-		echo '失败';
-	}
+	file_put_contents('./upfile.txt', $_POST);
 	
+	echo '上传成功';
 
 ?>
