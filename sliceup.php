53b5df5a03e60444bf5a2a06c1da0a417527ca41
Xingzhang Lin <1129507496@qq.com>
2016-12-22 23:46:02 +0800

    change file up

--- sliceup.php
+++ sliceup.php
@@ -2,16 +2,17 @@
 
 	// 接收文件，并合并
 	
-	var_dump($_FILES);
+	print_r($_POST);
+	print_r($_FILES);
 	
 	// 文件是否存在
-	if ( !file_exists('up.avi') ) {
-		move_uploaded_file($_FILES['part']['tmp_name'], './upload/up.avi');
-	} else {
-		// 存在文件合并
-		file_put_contents('./upload/up.avi', file_get_contents($_FILES['part']['tmp_name']), FILE_APPEND);
-	}
+//	if ( !file_exists('up.avi') ) {
+//		move_uploaded_file($_FILES['part']['tmp_name'], './upload/up.avi');
+//	} else {
+//		// 存在文件合并
+//		file_put_contents('./upload/up.avi', file_get_contents($_FILES['part']['tmp_name']), FILE_APPEND);
+//	}
 
-	echo 'ok';
+//	echo 'ok';
 	
 ?>
